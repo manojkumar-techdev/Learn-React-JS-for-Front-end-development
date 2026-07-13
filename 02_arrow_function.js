9f896de4d49d61c4599cf4f5a0d91a450a83e833
Manoj Kumar <contact@manojkumar-dev.com>
2026-07-13 23:52:55 +0200

    Update 02_arrow_function.js

--- 02_arrow_function.js
+++ 02_arrow_function.js
@@ -71,3 +71,14 @@ console.log(greet("Sam"));
 const numbers = [1, 2, 3, 4];
 const doubled = numbers.map(n => n * 2);
 console.log(doubled);
+
+
+// arrow function -- "this" is not bound to object
+const person = {
+  name: "Alice",
+  sayHello: () => {
+    console.log(this.name);
+  }
+};
+
+person.sayHello(); // Output: undefined
