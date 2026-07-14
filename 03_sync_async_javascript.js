ac3ac7d6d82c497049d93a5364349222ea3c1c41
Manoj Kumar <contact@manojkumar-dev.com>
2026-07-14 23:29:08 +0200

    Update 03_sync_async_javascript.js

--- 03_sync_async_javascript.js
+++ 03_sync_async_javascript.js
@@ -3,3 +3,14 @@ console.log("Step 1: Get bread");
 console.log("Step 2: Add filling");
 console.log("Step 3: Put top slice");
 console.log("Step 4: Eat sandwich!");
+
+
+// callbacks - the old way
+console.log("Timer Start");
+
+// Tell setTimeout to call our function AFTER 2000 milliseconds (2 seconds)
+setTimeout(() => {
+  console.log("Inside Timeout! This runs later.");
+}, 2000);
+
+console.log("Timer End (but timeout is still waiting)");
