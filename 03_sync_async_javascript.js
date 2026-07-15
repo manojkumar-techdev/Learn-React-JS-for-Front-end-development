363c2dcaad247f9f18c0563d5b77b763a598158a
Manoj Kumar <contact@manojkumar-dev.com>
2026-07-15 23:45:29 +0200

    Update 03_sync_async_javascript.js

--- 03_sync_async_javascript.js
+++ 03_sync_async_javascript.js
@@ -143,3 +143,15 @@ async function getData() {
 }
 
 getData();
+
+
+// promise example
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Data received");
+  }, 2000);
+});
+
+promise.then((data) => {
+  console.log(data);
+});
