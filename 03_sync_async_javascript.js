3dc0d057ef331f9e2fe0a75316b32a6b27a08bdc
Manoj Kumar <contact@manojkumar-dev.com>
2026-07-15 23:42:28 +0200

    Update 03_sync_async_javascript.js

--- 03_sync_async_javascript.js
+++ 03_sync_async_javascript.js
@@ -84,3 +84,21 @@ setTimeout(() => {
 }, 0); // Note the 0 millisecond delay
 
 console.log("C");
+
+
+// async and await
+async function getUserPosts(userId) {
+  try {
+    console.log("Fetching user...");
+    const user = await fetchUser(userId); // Takes 1 second
+    console.log("Fetching posts...");
+    const posts = await fetchPosts(user.id); // Takes 1 second
+    console.log("Done");
+    return posts;
+  } catch (error) {
+    console.error("Failed:", error);
+  }
+}
+
+getUserPosts(123);
+console.log("Function called");
