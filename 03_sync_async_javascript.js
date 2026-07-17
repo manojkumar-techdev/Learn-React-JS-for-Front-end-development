6968bf14c08d41297565b0778898dca190868922
Manoj Kumar <contact@manojkumar-dev.com>
2026-07-17 22:30:29 +0200

    Update 03_sync_async_javascript.js

--- 03_sync_async_javascript.js
+++ 03_sync_async_javascript.js
@@ -307,3 +307,18 @@ const userProfile = {
     city: 'London',
     ...contactInfo
 };
+
+
+// object destructuring
+const person = {
+  firstName: 'Alice',
+  age: 30,
+  city: 'Wonderland'
+};
+
+// To get the name and age into variables:
+const firstName = person.firstName;
+const age = person.age;
+
+console.log(firstName); // Output: Alice
+console.log(age);       // Output: 30
