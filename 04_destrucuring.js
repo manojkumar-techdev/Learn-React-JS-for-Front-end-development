4a3fd199b9cc0016e5f174ac816f36178020a4e0
Manoj Kumar <contact@manojkumar-dev.com>
2026-07-18 21:45:44 +0000

    Object Destructuring

---
+++ 04_destrucuring.js
@@ -0,0 +1,13 @@
+// Object Destrucuring
+const person = {
+  firstName: 'Alice',
+  age: 30,
+  city: 'Wonderland'
+};
+
+// To get the name and age into variables:
+const firstName = person.firstName;
+const age = person.age;
+
+console.log(firstName); // Output: Alice
+console.log(age);       // Output: 30
